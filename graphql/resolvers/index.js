76e95473cac2317d7f32da779a3967197ed5547c
erik antony <erik.antony.mg@gmail.com>
2022-01-05 10:37:46 -0500

    version 7.0

---
+++ graphql/resolvers/index.js
@@ -0,0 +1,23 @@
+const postsResolvers = require('./posts');
+const userResolvers = require('./user')
+const commentsResolvers = require('./comments')
+
+module.exports={
+    Post: {
+        likeCount(parent){
+            return parent.likes.length
+        },
+        commentCount:(parent)=> parent.comments.length
+    },
+    Query: {
+        ...postsResolvers.Query
+    },
+    Mutation:{
+        ...userResolvers.Mutation,
+        ...postsResolvers.Mutation,
+        ...commentsResolvers.Mutation
+    },
+    Subscription:{
+        ...postsResolvers.Subscription
+    }
+}
